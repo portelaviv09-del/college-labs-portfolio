9dff3a338306135063d24e99309c7f306e68dc41
portelaviv09-del <portelaviv09@gmail.com>
2026-02-06 04:53:31 -0700

    debugged this file

---
+++ python/debugging.py
@@ -0,0 +1,24 @@
+#Debug this code to make it run without errors and print the correct output.
+
+#butterflies = 10
+#beetles = 12
+#ladybugs = 20
+
+#print('I caught ' + butterflies + ' 🦋 butterflies!')
+#print('I caught ' + beetles + ' 🪲 beetles!'
+#print('I caught ' + ladybugs + ' 🐞 ladybugs!)
+
+#print('I caught ' + str(total) + ' total bugs!')
+
+#Debugged code:
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲  beetles!')
+print('I caught ' + str(ladybugs) + ' 🐞 ladybugs!')
+
+total = butterflies + beetles + ladybugs
+
+print('I caught ' + str(total) + ' total bugs!')
